e63d8a74c402f22171ee11b93645b4384a5a6d1b
Bagglebob <fawad-arshad96@hotmail.com>
2024-04-09 14:26:54 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,139 @@
+// JWT imports
+const jwt = require('jsonwebtoken');
+const passportJWT = require("passport-jwt");
+const passport = require("passport");
+
+const express = require('express');
+const app = express();
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+const userService = require("./user-service.js");
+
+// JWT strategy
+let ExtractJwt = passportJWT.ExtractJwt;
+let JwtStrategy = passportJWT.Strategy;
+// Configure its options
+let jwtOptions = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('jwt'),
+    secretOrKey: "4&sMdW$1FZ0m3o#W%n1,45M,oHko`44J9J8$d}PZhs\&*1|$R£m2P9GSnlCs£[%8",
+  };   
+  
+  let strategy = new JwtStrategy(jwtOptions, function (jwt_payload, next) {
+    console.log('payload received', jwt_payload);
+  
+    if (jwt_payload) {
+      // The following will ensure that all routes using
+      // passport.authenticate have a req.user._id, req.user.userName, req.user.fullName & req.user.role values
+      // that matches the request payload data
+      next(null, {
+        _id: jwt_payload._id,
+        userName: jwt_payload.userName,        
+      });
+    } else {
+      next(null, false);
+    }
+  });
+  
+const HTTP_PORT = process.env.PORT || 8080;
+
+// tell passport to use our "strategy"
+passport.use(strategy);
+
+// add passport as application-level middleware
+app.use(passport.initialize());
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/api/user/register", (req, res) => {
+    userService.registerUser(req.body)
+    .then((msg) => {
+        res.json({ "message": msg });
+    }).catch((msg) => {
+        res.status(422).json({ "message": msg });
+    });
+});
+
+app.post("/api/user/login", (req, res) => {
+// Check if the user is valid
+    userService.checkUser(req.body)
+    .then((user) => {
+        // generate a "payload" object consisting of two properties
+        let payload = {
+            _id: user._id,
+            userName: user.userName,    
+          };
+          // Sign the payload using "jwt"
+          let token = jwt.sign(payload, jwtOptions.secretOrKey);
+        // Once you have your signed token, include it as a "token" property within the JSON "message"
+        res.json({ "message": "login successful", "token": token });
+    }).catch(msg => {
+        res.status(422).json({ "message": msg });
+    });
+});
+
+app.get("/api/user/favourites", passport.authenticate('jwt', { session: false }), (req, res) => {
+    userService.getFavourites(req.user._id)
+    .then(data => {
+        res.json(data);
+    }).catch(msg => {
+        res.status(422).json({ error: msg });
+    })
+
+});
+
+app.put("/api/user/favourites/:id", passport.authenticate('jwt', { session: false }), (req, res) => {
+    userService.addFavourite(req.user._id, req.params.id)
+    .then(data => {
+        res.json(data)
+    }).catch(msg => {
+        res.status(422).json({ error: msg });
+    })
+});
+
+app.delete("/api/user/favourites/:id", passport.authenticate('jwt', { session: false }), (req, res) => {
+    userService.removeFavourite(req.user._id, req.params.id)
+    .then(data => {
+        res.json(data)
+    }).catch(msg => {
+        res.status(422).json({ error: msg });
+    })
+});
+
+app.get("/api/user/history", passport.authenticate('jwt', { session: false }), (req, res) => {
+    userService.getHistory(req.user._id)
+    .then(data => {
+        res.json(data);
+    }).catch(msg => {
+        res.status(422).json({ error: msg });
+    })
+
+});
+
+app.put("/api/user/history/:id", passport.authenticate('jwt', { session: false }), (req, res) => {
+    userService.addHistory(req.user._id, req.params.id)
+    .then(data => {
+        res.json(data)
+    }).catch(msg => {
+        res.status(422).json({ error: msg });
+    })
+});
+
+app.delete("/api/user/history/:id", passport.authenticate('jwt', { session: false }), (req, res) => {
+    userService.removeHistory(req.user._id, req.params.id)
+    .then(data => {
+        res.json(data)
+    }).catch(msg => {
+        res.status(422).json({ error: msg });
+    })
+});
+
+userService.connect()
+.then(() => {
+    app.listen(HTTP_PORT, () => { console.log("API listening on: " + HTTP_PORT) });
+})
+.catch((err) => {
+    console.log("unable to start the server: " + err);
+    process.exit();
+});
